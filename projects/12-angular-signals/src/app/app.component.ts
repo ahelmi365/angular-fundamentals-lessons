70054cb1e243149948c14c5acf1ee9ebb1921711
Ali Helmi <ahelmi365@gmail.com>
2024-04-06 15:50:31 +0200

    signals in angular

--- projects/12-angular-signals/src/app/app.component.ts
+++ projects/12-angular-signals/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Todo } from './todo';
 
@@ -6,9 +6,66 @@ import { Todo } from './todo';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
-  template: ` <h1>Building a TODO List</h1> `,
+  template: `
+    <h1>Building a TODO List</h1>
+    <p>Total number of completed tasks: {{ totalCompleted }}</p>
+    @for (todo of todos(); track todo.id) {
+    <label
+      for="{{ todo.id }}"
+      [ngStyle]="{
+        'text-decoration': todo.completed ? 'line-through' : 'none'
+      }"
+    >
+      <input
+        type="checkbox"
+        name="{{ todo.title }}"
+        id="{{ todo.id }}"
+        (change)="updateTodo(todo)"
+        (change)="conutCompletedItems(this.todos())"
+      />
+      {{ todo.title }}
+    </label>
+
+    }
+  `,
   styles: `label { display: block }`,
 })
 export class AppComponent {
-  updateTodo(todo: Todo) {}
+  todos = signal<Todo[]>([
+    {
+      id: 1,
+      title: 'Learn Angular',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Learn TypeScript',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Learn RxJS',
+      completed: false,
+    },
+  ]);
+  totalCompleted: number = 0;
+  updateTodo(todo: Todo) {
+    // todo.completed = !todo.completed;
+    this.todos.update((todos) => {
+      const uddated = todos.map((itme) => {
+        if (itme.id === todo.id) {
+          itme.completed = !itme.completed;
+        }
+        return itme;
+      });
+
+      return uddated;
+    });
+  }
+
+  conutCompletedItems(todos: Todo[]) {
+    this.totalCompleted = todos.reduce((previous, current) => {
+      return current.completed ? previous + 1 : previous;
+    }, 0);
+  }
 }
